301c549811f9f70c2e2864540aeeb0d0f39d7dc1
vinicius loureiro <viniciusloureiro@viniciusloureiro.local>
2014-11-17 17:37:57 -0200

    loop

--- js/livesearch.js
+++ js/livesearch.js
@@ -17,7 +17,7 @@ var searchFld = replaceSpecialChars($('#search').val());
 
 		if (searchFld.length) {
 			var searchTm = new RegExp(searchFld, "i");
-			var found = 0;
+			var valsearch = 0;
 
 	 $.getJSON('suggest.json', function (data) {
 
@@ -25,9 +25,8 @@ var searchFld = replaceSpecialChars($('#search').val());
 
 		 $.each(data.hightlights, function(i, item) {
 			//console.log(searchFld);
-				if (item.title.search(searchTm) != -1) {
-						 found = 1;
-					console.log(item.queries);
+				if (item.queries[i].search(searchTm) != -1) {
+						 valsearch = 1;
 					output += '<li class="header-suggestion">';
 					output += '<a class="logo-suggestion" href="' + item.url + '" target="_blank" ><img src="' + item.logo + ' " ></a> ';
 					output += '<a class="title-suggestion"  href="' + item.url + '" target="_blank" >' + item.title + '</a>';
@@ -38,14 +37,14 @@ var searchFld = replaceSpecialChars($('#search').val());
 		 output += '<li id="suggestion-type"> Sugestões de busca</li>';
 		 $.each(data.suggestions, function(i, item) {
 				if (item.search(searchTm) != -1) {
-						 found = 1;
+						 valsearch = 1;
 				output += '<li class="suggestion-more"> <a href="http://g1.globo.com/busca/?q=' + item +' " >' + item + '</a></li>';
 				}
 		 });
 		 output += '<li class="suggestion-searcher on-globo"> <a href="http://g1.globo.com/busca/?q=' + searchFld + '"> buscar &#39;<span>' + searchFld + '</span>&#39; na Globo.com <b>&rsaquo;</b> </a></li>';
 		 output += '<li class="suggestion-searcher on-web"> <a href="https://www.google.com.br/#q=' + searchFld + '"> buscar &#39;<span>' + searchFld + '</span>&#39; na Web <b>&rsaquo;</b> </a></li>';
 		output += '</ul>';
-		if (found==1) {
+		if (valsearch==1) {
 			$('#suggestions').removeClass('suggestions-hidden');
 			$('#suggestions').html(output);
 		}
