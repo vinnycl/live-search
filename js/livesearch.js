a390fa98e2a2a7c02a5db5f632734323315a3c2b
unknown <vinicius.loureiro@EW7OL1771.arca.corp.br>
2014-11-18 09:03:40 -0200

    merge

--- js/livesearch.js
+++ js/livesearch.js
@@ -1,61 +1,51 @@
-$('#search').keyup(function () {
-
-
 function replaceSpecialChars(str) {
-	str = str.replace(/[ÀÁÂÃÄÅ]/,"A");
-	str = str.replace(/[àáâãäå]/,"a");
-	str = str.replace(/[ÈÉÊË]/,"E");
-	str = str.replace(/[í]/,"i");
-	str = str.replace(/[Í]/,"I");
-	str = str.replace(/[ú]/,"u");
-	str = str.replace(/[Ú]/,"U");
-	str = str.replace(/[Ç]/,"C");
-	str = str.replace(/[ç]/,"c");
+	str = str.replace(/[àáâãäåÀÁÂÃÄÅ]/,"a");
+	str = str.replace(/[ÈÉÊËé]/,"e");
+	str = str.replace(/[íÍ]/,"i");
+	str = str.replace(/[úÚ]/,"u");
+	str = str.replace(/[çÇ]/,"c");
 	return str.replace(/[^a-z0-9]/gi,'');
 }
 
-var searchFld = replaceSpecialChars($('#search').val());
+$('#search').keyup(function () {
+	var valatSearc = $('#search').val();
+	var searchFld = replaceSpecialChars(valatSearc);
 
-		if (searchFld.length) {
-			var searchTm = new RegExp(searchFld, "i");
-			var found = 0;
+	if (searchFld.length) {
+		var searchTm = new RegExp(searchFld, "i");
+		var valsearch = 0;
 
-	 $.getJSON('suggest.json', function (data) {
+		$.getJSON('suggest.json', function (data) {
 
-		 output = '<ul class="searchresults">';
+			output = '<ul class="searchresults">';
 
-		 $.each(data.hightlights, function(i, item) {
-			console.log(searchFld);
-				if (item.title.search(searchTm) != -1) {
-						 found = 1;
-					//console.log(item.queries);
+			$.each(data.hightlights, function(i, item) {
+				if (item.queries[i].search(searchTm) != -1) {
+					valsearch = 1;
 					output += '<li class="header-suggestion">';
 					output += '<a class="logo-suggestion" href="' + item.url + '" target="_blank" ><img src="' + item.logo + ' " ></a> ';
 					output += '<a class="title-suggestion"  href="' + item.url + '" target="_blank" >' + item.title + '</a>';
-
 					output += '</li>';
 				}
-		 });
-		 output += '<li id="suggestion-type"> Sugestões de busca</li>';
-		 $.each(data.suggestions, function(i, item) {
+		 	});
+			output += '<li id="suggestion-type">Sugestões de busca</li>';
+			$.each(data.suggestions, function(i, item) {
 				if (item.search(searchTm) != -1) {
-						 found = 1;
-				output += '<li class="suggestion-more"> <a href="http://g1.globo.com/busca/?q=' + item +' " >' + item + '</a></li>';
+					valsearch = 1;
+					output += '<li class="suggestion-more"> <a href="http://g1.globo.com/busca/?q=' + item +' " >' + item + '</a></li>';
 				}
-		 });
-		 output += '<li class="suggestion-searcher on-globo"> <a href="http://g1.globo.com/busca/?q=' + searchFld + '"> buscar &#39;<span>' + searchFld + '</span>&#39; na Globo.com <b>&rsaquo;</b> </a></li>';
-		 output += '<li class="suggestion-searcher on-web"> <a href="https://www.google.com.br/#q=' + searchFld + '"> buscar &#39;<span>' + searchFld + '</span>&#39; na Web <b>&rsaquo;</b> </a></li>';
-		output += '</ul>';
-		if (found==1) {
-			$('#suggestions').removeClass('suggestions-hidden');
-			$('#suggestions').html(output);
-		}
-		else {
+			 });
+			output += '<li class="suggestion-searcher on-globo"> <a href="http://g1.globo.com/busca/?q=' + searchFld + '"> buscar &#39;<span>' + valatSearc + '</span>&#39; na Globo.com <b>&rsaquo;</b> </a></li>';
+			output += '<li class="suggestion-searcher on-web"> <a href="https://www.google.com.br/#q=' + searchFld + '"> buscar &#39;<span>' + valatSearc + '</span>&#39; na Web <b>&rsaquo;</b> </a></li>';
+			output += '</ul>';
+			if (valsearch==1) {
+				$('#suggestions').removeClass('suggestions-hidden');
+				$('#suggestions').html(output);
+			} else {
 				$('#suggestions').addClass('suggestions-hidden');
-		}
-});
-		}
-		else {
-			$('#suggestions').addClass('suggestions-hidden');
-		}
+			}
+		});
+	} else {
+		$('#suggestions').addClass('suggestions-hidden');
+	}
 });
